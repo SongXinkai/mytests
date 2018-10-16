a16b841b9916b94feaa93f84b7dc44ce6f9f6541
SongXinkai <sxk018@163.com>
2018-10-16 14:47:59 +0000

    20181016

--- c++time/main.cpp
+++ c++time/main.cpp
@@ -9,10 +9,18 @@ int main(){
   if (a == 0){
     cout << "xx" << endl;
   }
-  time(&a);
-  sleep(4);
+  time(&a); // time.h
+  sleep(4); // unistd.h
   time(&b);
   cout << a << ", " << b << endl;
   cout << b -a << endl;
+
+  clock_t clc_a = 0, clc_b = 0;
+  clc_a=clock(); // time.h
+  sleep(4); // unistd.h
+  clc_b=clock();
+  cout << clc_a << ", " << clc_b <<", CLOCKS_PER_SEC: " << CLOCKS_PER_SEC<< endl;
+  cout << (clc_b - clc_a) / CLOCKS_PER_SEC << endl;
+
   return 0;
 }
